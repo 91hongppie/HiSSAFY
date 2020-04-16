1e035d8620b36083a58c376adb82cdd1f7136536
조선행 <mostraw009@gmail.com>
2020-04-16 19:04:12 +0900

    #S02P23B106-13 | -ing | Make layout, add chart(-ing)

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,39 +1,53 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        front
-      </h1>
-      <h2 class="subtitle">
-        AI Project - front
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <video autoplay="true">No video support in your browser</video>
+    <img src="">
+    <canvas style="display:none;" width="640" height="480"></canvas>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
+  beforeMount () {
+    this.getVideo()
+  },
+  methods: {
+    getVideo () {
+      const constraints = { audio: false, video: true }
+      const video = document.querySelector('video')
+      const canvas = document.querySelector('canvas')
+      const context = canvas.getContext('2d')
+      let localMediaStream = null
+
+      function snapshot () {
+        if (localMediaStream) {
+          const image = document.querySelector('img')
+          context.drawImage(video, 0, 0, 640, 480)
+          image.src = canvas.toDataURL('image/png')
+        }
+      }
+      video.addEventListener('click', snapshot, false)
+
+      navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+          // Older browsers may not have srcObject
+          if ('srcObject' in video) {
+            video.srcObject = stream
+            localMediaStream = stream
+          } else {
+            // Avoid using this in new browsers, as it is going away.
+            video.src = window.URL.createObjectURL(stream)
+            localMediaStream = stream
+          }
+          video.onloadedmetadata = function (e) {
+            video.play()
+          }
+        })
+        .catch(function (err) {
+          alert(err.name + ': ' + err.message)
+        })
+    }
   }
 }
 </script>
