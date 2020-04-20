ac5391667cb14be8c98d895b23f35b4e68507922
91hongppie <91hongppie@gmail.com>
2020-04-20 17:28:05 +0900

    S02P23B106-61 | -ing | Test face_recognition

--- back/arti_intelli/serializers.py
+++ back/arti_intelli/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Campus, Account, Check, Face
+from .models import Campus, Account, Check
 
 class CampusSerializer(serializers.ModelSerializer):
     class Meta(Campus):
@@ -20,8 +20,8 @@ class CheckSerializer(serializers.ModelSerializer):
         fields = ('id', 'date', 'in_time', 'out_time', 'is_late', 'is_early_left', 'status', 'student_info_id', 'account',)
 
 
-class FaceSerializer(serializers.ModelSerializer):
-    class Meta(Face):
-        model = Face
-        fields = ('id', 'pic_name', 'top', 'bottom', 'right', 'left', 'account',)
+# class FaceSerializer(serializers.ModelSerializer):
+#     class Meta(Face):
+#         model = Face
+#         fields = ('id', 'pic_name', 'top', 'bottom', 'right', 'left', 'account',)
 
