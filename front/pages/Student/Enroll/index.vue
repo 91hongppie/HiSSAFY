6f4321d7205325df7568cd71dde3c2af7f747c1f
ghleex <ghleex@gmail.com>
2020-04-27 13:23:56 +0900

    S02P23B106-43 | -ED | Modifying codes

--- front/pages/Student/Enroll/index.vue
+++ front/pages/Student/Enroll/index.vue
@@ -108,6 +108,34 @@ export default {
       // localstream.getTracks()[0].stop()
       alert('Video off')
     },
+    showTime () {
+      const date = new Date()
+      let h = date.getHours() // 0 - 23
+      let m = date.getMinutes() // 0 - 59
+      let s = date.getSeconds() // 0 - 59
+      // let session = 'AM'
+
+      // if (h === 0) {
+      //   h = 12
+      // }
+
+      // if (h > 12) {
+      //   h = h - 12
+      //   session = 'PM'
+      // } else if (h === 12) {
+      //   session = 'PM'
+      // }
+
+      h = (h < 10) ? '0' + h : h
+      m = (m < 10) ? '0' + m : m
+      s = (s < 10) ? '0' + s : s
+
+      // const time = h + ':' + m + ':' + s + ' ' + session
+      const time = h + ':' + m + ':' + s
+      document.getElementById('ClockDisplay').textContent = time
+
+      setTimeout(this.showTime, 1000)
+    },
     hideShow () {
       const btns = document.getElementById('btns')
       const video = document.querySelector('video')
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -35,24 +35,25 @@ export default {
       let h = date.getHours() // 0 - 23
       let m = date.getMinutes() // 0 - 59
       let s = date.getSeconds() // 0 - 59
-      let session = 'AM'
+      // let session = 'AM'
 
-      if (h === 0) {
-        h = 12
-      }
+      // if (h === 0) {
+      //   h = 12
+      // }
 
-      if (h > 12) {
-        h = h - 12
-        session = 'PM'
-      } else if (h === 12) {
-        session = 'PM'
-      }
+      // if (h > 12) {
+      //   h = h - 12
+      //   session = 'PM'
+      // } else if (h === 12) {
+      //   session = 'PM'
+      // }
 
       h = (h < 10) ? '0' + h : h
       m = (m < 10) ? '0' + m : m
       s = (s < 10) ? '0' + s : s
 
-      const time = h + ':' + m + ':' + s + ' ' + session
+      // const time = h + ':' + m + ':' + s + ' ' + session
+      const time = h + ':' + m + ':' + s
       document.getElementById('ClockDisplay').textContent = time
 
       setTimeout(this.showTime, 1000)
