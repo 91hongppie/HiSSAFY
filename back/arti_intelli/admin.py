915787e6e90816856b45353e226772850ca40571
johh201200 <johh201200@gmail.com>
2020-04-27 18:59:57 +0900

    S02P23B106-28 | -ing | dev

--- back/arti_intelli/admin.py
+++ back/arti_intelli/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Campus, Account, Check
+from .models import Campus, Account, Check, AccountImage
 
 # Register your models here.
 class CampusAdmin(admin.ModelAdmin):
@@ -13,8 +13,13 @@ admin.site.register(Account, AccountAdmin)
 
 
 class CheckAdmin(admin.ModelAdmin):
-    list_display = ('id', 'date', 'in_time', 'out_time', 'is_late', 'is_early_left', 'status')
+    list_display = ('id', 'date', 'in_time', 'out_time', 'is_late', 'is_early_left', 'status', 'student_info_id',)
 admin.site.register(Check, CheckAdmin)
+
+
+class AccountImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'pic_name', 'name', 'student_info_id',)
+admin.site.register(AccountImage, AccountImageAdmin)
 
 
 # class FaceAdmin(admin.ModelAdmin):
