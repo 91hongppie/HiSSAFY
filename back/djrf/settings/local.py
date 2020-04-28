e3994647a7d73355d25a568ec4a81eae6cbbd19c
johh201200 <johh201200@gmail.com>
2020-04-28 12:21:43 +0900

    -ing | dev

--- back/djrf/settings/local.py
+++ back/djrf/settings/local.py
@@ -10,14 +10,24 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # use this when mysql is the basic db
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'ai',
+#         'USER': 'root',
+#         'PASSWORD': '1324adsf',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ai',
-        'USER': 'root',
+        'USER': 'user',
         'PASSWORD': '1324adsf',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
 }
-
