b34a3b3f3851e2ab8064fa02fe7f053e5118fa63
조선행 <mostraw009@gmail.com>
2020-04-29 14:45:37 +0900

    #S02P23B106-75 | -ing | Branch 1 or many

--- front/pages/main/students/index/_studentList.vue
+++ front/pages/main/students/index/_studentList.vue
@@ -1,18 +1,14 @@
 <template>
   <div>
-    <v-container v-if="resultList.length === 1">
-      <v-row>
-        <v-col>
+    <!-- 1명일 때 -->
+    <v-container v-if="resultList.length === 1" class="nanumG-20">
+      <v-row class="d-flex justify-center align-center jua-25">
+        <v-chip color="blue lighten-2" class="mx-5" style="color: white;">
           {{ locations[resultList[0].region] }} {{ resultList[0].stage }}기
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="font-weight-bold">
-          이름
-        </v-col>
-        <v-col>
+        </v-chip>
+        <a class="non-a">
           {{ resultList[0].name }}
-        </v-col>
+        </a>
       </v-row>
 
       <v-row class="text-center">
@@ -78,81 +74,19 @@
         </v-col>
       </v-row>
     </v-container>
-    <v-container v-else v-for="student in resultList" :key="student.id" class="text-center studentCard">
-      <v-row>
-        <v-col>
-          {{ locations[student.region] }} {{ student.stage }}기
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="font-weight-bold">
-          이름
-        </v-col>
-        <v-col>
-          {{ student.name }}
-        </v-col>
-      </v-row>
 
-      <v-row class="text-center">
-        <v-col class="font-weight-bold">
-          출석률 (%)
+    <!-- 1명 이상일 때 -->
+    <v-container v-else class="text-center">
+      <v-row v-for="studentIndex in Math.ceil(resultList.length / 2)" :key="studentIndex">
+        <v-col cols="6">
+          <v-container class="studentCard">
+            {{ resultList[(studentIndex - 1) * 2]}}
+          </v-container>
         </v-col>
-        <v-col>
-          {{ student.attendance_rate }}
-        </v-col>
-      </v-row>
-
-      <v-row class="text-center">
-        <v-col class="font-weight-bold">
-          평균 입실시간
-        </v-col>
-        <v-col>
-          {{ student.intime }}
-        </v-col>
-      </v-row>
-
-      <v-row class="text-center">
-        <v-col class="font-weight-bold">
-          평균 퇴실시간
-        </v-col>
-        <v-col>
-          {{ student.outtime }}
-        </v-col>
-      </v-row>
-
-      <v-row class="text-center">
-        <v-col class="font-weight-bold">
-          지각 (회)
-        </v-col>
-        <v-col>
-          {{ student.come_late_cnt }}
-        </v-col>
-      </v-row>
-
-      <v-row class="text-center">
-        <v-col class="font-weight-bold">
-          조퇴 (회)
-        </v-col>
-        <v-col>
-          {{ student.early_left_cnt }}
-        </v-col>
-      </v-row>
-
-      <v-row class="text-center">
-        <v-col class="font-weight-bold">
-          사유 결석 (회)
-        </v-col>
-        <v-col>
-          {{ student.allow_absent_day }}
-        </v-col>
-      </v-row>
-
-      <v-row class="text-center">
-        <v-col class="font-weight-bold">
-          임의 결석 (회)
-        </v-col>
-        <v-col>
-          {{ student.Disallow_absent_day }}
+        <v-col cols="6">
+          <v-container class="studentCard">
+            {{ resultList[(studentIndex - 1) * 2 + 1]}}
+          </v-container>
         </v-col>
       </v-row>
     </v-container>
@@ -161,56 +95,97 @@
 
 <script>
 export default {
-  async asyncData ({ params, $axios }) {
-    const conditions = params.studentList.split('&')
-    let location = ''
-    let name = ''
-    if (conditions.length > 1) {
-      console.log(conditions)
-      if (conditions[0] === '') {
-        console.log('이름만!')
-        name = conditions[1]
-      } else {
-        console.log('둘다!!')
-        location = conditions[0]
-        name = conditions[1]
-      }
-    } else {
-      console.log('지역!!!!!')
-      location = conditions[0]
-      console.log('wowo')
-    }
+  // async asyncData ({ params, $axios }) {
+  //   const conditions = params.studentList.split('&')
+  //   let location = ''
+  //   let name = ''
+  //   if (conditions.length > 1) {
+  //     console.log(conditions)
+  //     if (conditions[0] === '') {
+  //       console.log('이름만!')
+  //       name = conditions[1]
+  //     } else {
+  //       console.log('둘다!!')
+  //       location = conditions[0]
+  //       name = conditions[1]
+  //     }
+  //   } else {
+  //     console.log('지역!!!!!')
+  //     location = conditions[0]
+  //     console.log('wowo')
+  //   }
 
-    const today = new Date()
-    const year = today.getFullYear()
-    const month = today.getMonth() + 1
-    const studentData = await $axios.$get(`/api/checks/month/all/${year}/${month}`)
-    const resultList = []
-    if (location === '') {
-      for (const student in studentData) {
-        if (studentData[student].name.includes(decodeURI(name)) || studentData[student].student_id === name) {
-          resultList.push(studentData[student])
-        }
-      }
-    } else if (name === '') {
-      for (const student in studentData) {
-        if (location.includes(studentData[student].region)) {
-          resultList.push(studentData[student])
-        }
-      }
-    } else {
-      for (const student in studentData) {
-        if (location.includes(studentData[student].region) && (studentData[student].name.includes(decodeURI(name)) || studentData[student].student_id === Number(name))) {
-          resultList.push(studentData[student])
-        }
-      }
-    }
-    console.log(resultList)
-    return { year, month, resultList }
-  },
+  //   const today = new Date()
+  //   const year = today.getFullYear()
+  //   const month = today.getMonth() + 1
+  //   const studentData = await $axios.$get(`/api/checks/month/all/${year}/${month}`)
+  //   const resultList = []
+  //   if (location === '') {
+  //     for (const student in studentData) {
+  //       if (studentData[student].name.includes(decodeURI(name)) || studentData[student].student_id === name) {
+  //         resultList.push(studentData[student])
+  //       }
+  //     }
+  //   } else if (name === '') {
+  //     for (const student in studentData) {
+  //       if (location.includes(studentData[student].region)) {
+  //         resultList.push(studentData[student])
+  //       }
+  //     }
+  //   } else {
+  //     for (const student in studentData) {
+  //       if (location.includes(studentData[student].region) && (studentData[student].name.includes(decodeURI(name)) || studentData[student].student_id === Number(name))) {
+  //         resultList.push(studentData[student])
+  //       }
+  //     }
+  //   }
+  //   console.log(resultList)
+  //   return { year, month, resultList }
+  // },
   data () {
     return {
-      locations: ['', '서울', '대전', '광주', '구미']
+      locations: ['', '서울', '대전', '광주', '구미'],
+      resultList: [
+        {
+          region: 2,
+          stage: 1,
+          student_id: '0234999',
+          name: '홍길금',
+          attendance_rate: 100,
+          intime: '',
+          outtime: '',
+          come_late_cnt: 1,
+          early_left_cnt: 0,
+          allow_absent_day: 0,
+          Disallow_absent_day: 1
+        },
+        {
+          region: 2,
+          stage: 1,
+          student_id: '0234989',
+          name: '홍길은',
+          attendance_rate: 100,
+          intime: '',
+          outtime: '',
+          come_late_cnt: 1,
+          early_left_cnt: 0,
+          allow_absent_day: 0,
+          Disallow_absent_day: 1
+        },
+        {
+          region: 2,
+          stage: 1,
+          student_id: '0234980',
+          name: '홍길동',
+          attendance_rate: 100,
+          intime: '',
+          outtime: '',
+          come_late_cnt: 1,
+          early_left_cnt: 0,
+          allow_absent_day: 0,
+          Disallow_absent_day: 0
+        }
+      ]
     }
   }
 }
@@ -221,5 +196,17 @@ export default {
   border: 1.5px solid black;
   border-radius: 10px;
   margin: 5px;
+}
+.nanumG-20 {
+  font-family: 'Nanum Gothic', sans-serif;
+  font-size: 20px;
+}
+.jua-25 {
+  font-family: 'Jua', sans-serif;
+  font-size: 25px;
+}
+.non-a {
+  text-decoration: none;
+  color: black;
 }
 </style>
